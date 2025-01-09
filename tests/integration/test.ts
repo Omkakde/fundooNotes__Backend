f869eecbfaf26198af7d111046f93f1af7a82181
Omkakde <kakadeom123@gmail.com>
2025-01-09 16:11:03 +0530

    [Om] added Testing by using Mocha & Chai

--- tests/integration/test.ts
+++ tests/integration/test.ts
@@ -52,5 +52,73 @@ describe('Routes', () => {
     expect(response.body).to.have.property('newToken');
   });
   
-  
+  let noteId;
+  // Create a Note Test
+  it('should create a note', async () => {
+    const response = await request(app.getApp())
+      .post('/api/v1/notes/')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        title: 'My First Note',
+        description: 'This is my first note.',
+      });
+    expect(response.status).to.equal(201);
+    expect(response.body).to.have.property('message').that.includes('Note created successfully');
+  });
+  // Get All Notes Test
+  it('should get all notes', async () => {
+    const response = await request(app.getApp())
+      .get('/api/v1/notes/')
+      .set('Authorization', `Bearer ${accessToken}`);
+    noteId = response.body.data[0].id
+    expect(response.status).to.equal(200);
+    expect(response.body).to.be.an('Object');
+  });
+  // Get Note by ID Test
+  it('should get note by ID', async () => {
+    const response = await request(app.getApp())
+      .get(`/api/v1/notes/${noteId}`)
+      .set('Authorization', `Bearer ${accessToken}`);
+    expect(response.status).to.equal(200);
+    expect(response.body[0]).to.have.property('id').that.equals(noteId);
+  });
+  // Update Note Test
+  it('should update a note by ID', async () => {
+    const response = await request(app.getApp())
+      .put(`/api/v1/notes/${noteId}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        title: 'Updated Note',
+        description: 'This note has been updated.',
+      });
+    expect(response.status).to.equal(200);
+    expect(response.body).to.have.property('message').that.includes('Note updated');
+  });
+  // Archive Note Test
+  it('should archive a note by ID', async () => {
+    const response = await request(app.getApp())
+      .put(`/api/v1/notes/${noteId}/archive`)
+      .set('Authorization', `Bearer ${accessToken}`);
+    expect(response.status).to.equal(200);
+    expect(response.body).to.have.property('message').that.includes('Note archived');
+  });
+  // Trash Note Test
+  it('should trash a note by ID', async () => {
+    const response = await request(app.getApp())
+      .put(`/api/v1/notes/${noteId}/trash`)
+      .set('Authorization', `Bearer ${accessToken}`);
+    expect(response.status).to.equal(200);
+    expect(response.body).to.have.property('message').that.includes('Note moved to trash');
+  });
+
+
+
+  // Delete Note Test
+  it('should delete a note by ID', async () => {
+    const response = await request(app.getApp())
+      .delete(`/api/v1/notes/${noteId}`)
+      .set('Authorization', `Bearer ${accessToken}`);
+    expect(response.status).to.equal(200);
+    expect(response.body).to.have.property('message').that.includes('Note permanently deleted');
+  });
 });
